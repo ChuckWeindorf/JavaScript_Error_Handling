ff73f70c78e037183dc0a1bdc51eb03d136d9a4d
ChuckWeindorf <charles.weindorf@mudsox.com>
2024-01-26 15:55:18 -0500

    First try

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 // TODO: Enable strict mode
+"use strict";
 
 // TODO: Fix the following parameter list
-function parseToJSON(data, data) {
+function parseToJSON(data, data2) {
   // TODO: Add a try/catch block to
   // attempt to convert 'data' to JSON
-  return JSON.parse(data); 
+  try 
+     {return JSON.parse(data); }
+  catch (error)
+      {//console.error(error.message);
+      return null;}
+  finally {
+
+  }
   
   // TODO: if an exception is raised
   // print the error to the console
@@ -15,6 +23,9 @@ let failData = "<h1>Hello World!</h1>"
 
 let passData = JSON.stringify({ success: true });
 
-console.log(parseToJSON(failData));
-
-console.log(parseToJSON(passData));
+try {
+console.log(parseToJSON(failData));}
+catch (error2) {console.error(error2.message)}
+try{
+console.log(parseToJSON(passData));}
+catch (error3) {console.error(error3.message)}
